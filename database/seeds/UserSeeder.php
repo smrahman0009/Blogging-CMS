59f9216ff3c6cb38002deecd965cc1fd4783fa28
Mushfikur Rahman <35715315+smrahman0009@users.noreply.github.com>
2020-07-06 11:02:33 +0600

    Seeding Default Administrator

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(
+            [
+                'name' => 'miead',
+                'email' => 'miead@gmail.com',
+                'password' => bcrypt('password'),
+            ]
+        );                
+    }
+}
