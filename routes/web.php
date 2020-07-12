63ce2fdd4c6912beade1adff33d474ae4456a0ad
Mushfikur Rahman <35715315+smrahman0009@users.noreply.github.com>
2020-07-12 21:18:21 +0600

    display home page dynamically -> disdplay the first page dynamically

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','FrontEndController@index')->name('index');
 
 Route::get('/test/',function () {
     return Auth::user()->profile->is_admin;
